4125cee6ffe20eb16dd1f05470a78200f757b7bd
Ismael Tondi <smiletondi@gmail.com>
2020-02-13 23:21:07 +0100

    Add countdown header and countdown title

--- src/App.js
+++ src/App.js
@@ -4,7 +4,32 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <div>
+        <h3 className="countdown-title">Countdown Timer</h3>
+        <div className="countdown">
+          <div className="countdown-header">
+            <div className="countdown-form">
+              <form className="form-inline">
+                <div className="form-group">
+                  <h5 className="font-weight-bold count">Countdown: </h5>
+                  <input
+                    type="number"
+                    className="form-control"
+                    placeholder="(Min)"
+                    min="1"
+                  />
+                </div>
+                <div className="btn btn-form-start"> START </div>
+                {/* <div className="btn btn-form-reset"> RESET </div> */}
+              </form>
+              {/* <p className="form-error">
+                <span >error</span>
+              </p> */}
+            </div>
+          </div>
+          
+        </div>
+      </div >
     </div >
   );
 }
