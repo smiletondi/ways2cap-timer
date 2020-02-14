d3e2e5c26542c3b55fda1932d4392b872a90f952
Ismael Tondi <smiletondi@gmail.com>
2020-02-14 09:48:35 +0100

    Add the countdown body title and fix some typo

--- src/App.js
+++ src/App.js
@@ -28,19 +28,20 @@ function App() {
             </div>
           </div>
           <div className="countdown-body">
+            <h5 className="font-weight-bolder my-3">More than halfway there!</h5>
             <div className="countdown-body">
               <h3 className="countdown-total">
                 <span className="minutes">15</span>
                 <span className="separator">:</span>
                 <span className="seconds"> 31 </span>
               </h3>
-            </div>
-            <div className="countdown-stop-button">
-              <div className="btn btn-stop">
-                <i className="fa fa-pause"></i>
-              </div>
-              <div className="btn btn-start">
-                <i className="fa fa-play"></i>
+              <div className="countdown-play">
+                <div className="btn btn-stop">
+                  <i className="fa fa-pause"></i>
+                </div>
+                <div className="btn btn-start">
+                  <i className="fa fa-play"></i>
+                </div>
               </div>
             </div>
           </div>
