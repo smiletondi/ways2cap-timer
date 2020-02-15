ca41f8c46d81d681869021c3157bf9b94429de41
Ismael Tondi <smiletondi@gmail.com>
2020-02-15 04:51:45 +0100

    Updates

---
+++ precache-manifest.e2c13392b2c95ab496e21064de48815a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68e5aeee12985ca55c59cf4eeca27568",
+    "url": "/ways2cap-timer/index.html"
+  },
+  {
+    "revision": "27df9af418a3b022f4cf",
+    "url": "/ways2cap-timer/static/css/main.6f159583.chunk.css"
+  },
+  {
+    "revision": "7f6459f6bd77da11e113",
+    "url": "/ways2cap-timer/static/js/2.08f81eb8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/ways2cap-timer/static/js/2.08f81eb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27df9af418a3b022f4cf",
+    "url": "/ways2cap-timer/static/js/main.c0d52aad.chunk.js"
+  },
+  {
+    "revision": "6eb5063024c4a962a4fd",
+    "url": "/ways2cap-timer/static/js/runtime-main.ddaced87.js"
+  }
+]);
